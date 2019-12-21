23dde7d0187aa8f7271c4ab80cd9bb03ae99de0b
ssliteris <ssliteris@exadel.com>
2019-12-21 17:47:23 +0200

    feat/swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Ajax-BE API')
+    .setDescription('Nobody cares')
+    .setVersion('1.0')
+    .addTag('ajax')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
