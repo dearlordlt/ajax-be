dc80e17231cd307555f3dcb020a5b8afe6823681
Daumantas Girininkas <ssliteris@deloitte.com>
2020-04-12 20:23:52 +0300

    update auth/register endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -16,7 +16,7 @@ export class AppController {
   constructor(
     private readonly authService: AuthService,
     private readonly usersService: UsersService,
-  ) { }
+  ) {}
 
   @ApiBody({ type: UserDto })
   @Post('auth/login')
@@ -66,16 +66,16 @@ export class AppController {
     return this.usersService.createMany(users);
   }
 
-  @UseGuards(AuthGuard('jwt'))
   @ApiBody({ type: UserDto })
   @Post('auth/register')
   async registerUser(@Body() user: UserDto): Promise<any> {
     return this.authService.registerUser(user).then((data: IUser) => {
+      const { _id, username, email, role } = data;
       return {
-        _id: data._id,
-        username: data.username,
-        email: data.email,
-        role: data.role,
+        _id,
+        username,
+        email,
+        role,
       };
     });
   }
